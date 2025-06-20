cefdc93f075cce1192cbf9a9e8f622d3dd07d6b0
Michał Jamros <jamros.michal2020@gmail.com>
2025-06-20 12:33:37 +0200

    another fix

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 set -e
 
 echo "▶️ Uruchamianie migracji bazy danych..."
-echo "DEBUG: DATABASE_URL is $DATABASE_URL" && alembic upgrade head
-alembic -c foodtracker_app/alembic.ini upgrade head
+
+
+alembic -c /app/foodtracker_app/alembic.ini upgrade head
 
 echo "▶️ Startowanie serwera Uvicorn..."
+
 exec "$@"
