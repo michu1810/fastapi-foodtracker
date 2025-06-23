95a4244b0cb35e13da5ebd348c2134e7fa806639
Michał Jamros <jamros.michal2020@gmail.com>
2025-06-23 06:41:05 +0200

    add new endpoint in external file

--- foodtracker/foodtracker_app/external/routes.py
+++ foodtracker/foodtracker_app/external/routes.py
@@ -49,3 +49,42 @@ async def search_products_from_external_api(q: str = Query(..., min_length=3)):
                 )
 
     return results
+
+
+@router.get("/barcode/{barcode}")
+async def get_product_by_barcode(barcode: str):
+    if not barcode.isdigit():
+        raise HTTPException(
+            status_code=400, detail="Kod kreskowy musi składać się z cyfr"
+        )
+
+    PRODUCT_URL = f"https://world.openfoodfacts.org/api/v2/product/{barcode}.json"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(PRODUCT_URL)
+            response.raise_for_status()
+            data = response.json()
+        except httpx.RequestError as exc:
+            raise HTTPException(
+                status_code=503, detail=f"Błąd komunikacji z zewnętrznym API: {exc}"
+            )
+        except httpx.HTTPStatusError as exc:
+            if exc.response.status_code == 404:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Produkt o kodzie {barcode} nie został znaleziony",
+                )
+
+            product = data.get("product")
+            if not product:
+                raise HTTPException(
+                    status_code=404, detail="Brak danych produktow w odpowiedzi API."
+                )
+            result = {
+                "id": product.get("code"),
+                "name": product.get("product_name_pl") or product.get("product_name"),
+                "description": product.get("brands", "Brak informacji o marce"),
+                "image_url": product.get("image_front_url"),
+            }
+            return result
