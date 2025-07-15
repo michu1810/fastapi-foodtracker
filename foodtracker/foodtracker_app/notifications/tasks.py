20c570d47e1143c5690fff6cb84c6411b641056a
Michał Jamros <jamros.michal2020@gmail.com>
2025-07-15 23:01:49 +0200

    fix: change enviroment variable for database

--- foodtracker/foodtracker_app/notifications/tasks.py
+++ foodtracker/foodtracker_app/notifications/tasks.py
@@ -32,7 +32,7 @@ async def get_db_session(provided_session: AsyncSession | None = None):
 
     engine = None
     try:
-        engine = create_async_engine(settings.DATABASE_URL_ASYNCPG)
+        engine = create_async_engine(settings.DATABASE_URL)
         async_session_maker = sessionmaker(
             bind=engine, class_=AsyncSession, expire_on_commit=False
         )
