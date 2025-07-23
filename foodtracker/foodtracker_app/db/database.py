13e8f4b73bddbd16d3444aab2b650c843363203c
Michał Jamros <jamros.michal2020@gmail.com>
2025-07-23 22:59:57 +0200

    change for the new database service

--- foodtracker/foodtracker_app/db/database.py
+++ foodtracker/foodtracker_app/db/database.py
@@ -5,6 +5,7 @@ from foodtracker_app.settings import settings
 from sqlalchemy import TypeDecorator, DateTime
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.engine.url import make_url
 
 
 class TZDateTime(TypeDecorator):
@@ -24,9 +25,20 @@ class TZDateTime(TypeDecorator):
         return None
 
 
-DATABASE_URL = settings.DATABASE_URL
+raw_url = settings.DATABASE_URL
+url_object = make_url(raw_url)
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+if settings.IS_PRODUCTION:
+    url_object = url_object.update_query_pairs([("ssl", "require")])
+
+engine = create_async_engine(
+    url_object,
+    echo=True,
+    connect_args={
+        "statement_cache_size": 0,
+    },
+)
+
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 Base = declarative_base()
